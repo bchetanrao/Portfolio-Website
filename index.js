5002dce5db93416957b43af118c7a7eac847d35c
bchetanrao <chetanraocr7@gmail.com>
2021-11-27 13:34:54 +0530

    made transition smoother

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var loader = document.getElementById("loader");
 
-        window.addEventListener('load',()=>{
-          setTimeout(hideLoader,3000);
-        })
-        function hideLoader(){
-          loader.style.display = "none";
-        }
+window.addEventListener("load", () => {
+	setTimeout(hideLoader, 3000);
+});
+function hideLoader() {
+	loader.style.opacity = 0;
+}
 
 var animation = bodymovin.loadAnimation({
 	container: document.getElementById("bm"),
